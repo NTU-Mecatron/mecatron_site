fc093b56953a3ae6d4269303dfa0d6c1a69e1943
Tan Zi Xuan <tanzixuan88@gmail.com>
2025-07-13 22:50:09 +0800

    Edited Blender3DPage
    Added images as well

--- src/ComputerVisionPage.jsx
+++ src/ComputerVisionPage.jsx
@@ -19,7 +19,7 @@ export default function ComputerVisionPage() {
               alt="Photo"
               caption="100% focused on powering the robot"
               rotate="-rotate-6"
-              photocolour='#EA580C'
+              photocolour='#ffffffff'
             />
           </div>
 
@@ -47,7 +47,7 @@ export default function ComputerVisionPage() {
               alt="Photo 3"
               caption="Another fully-focused team!"
               rotate="rotate-3"
-              photocolour='#EA580C'
+              photocolour='#ffffffff'
             />
           </div>
 
@@ -135,7 +135,7 @@ function PolaroidPhoto({ src, alt, caption, rotate = '-rotate-3', photocolour = 
         className="w-96 aspect-[5/3] object-cover mx-auto rounded-md"
       />
       {caption && (
-        <p className="text-white mt-2 text-sm italic font-bold">{caption}</p>
+        <p className="text-orange-600 mt-2 text-sm italic font-bold">{caption}</p>
       )}
     </div>
   );
